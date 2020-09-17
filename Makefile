b255f0d49c02e9db75e297d0464ffdae1b421f31
Andrew Beekhof <andrew@beekhof.net>
2020-09-17 12:56:07 +1000

    Support making multiple AIO clusters

--- Makefile
+++ Makefile
@@ -1,21 +1,37 @@
-clean:
-	./hack/virt-delete-aio.sh || true
-	rm -rf mydir
+clean-%:
+	sudo virsh destroy $* || true
+	sudo virsh undefine $* || true
 
-generate:
-	mkdir mydir
-	cp ./install-config.yaml mydir/
-	./bin/openshift-install create aio-config --dir=mydir
+./bin/openshift-install:
+	TAGS=libvirt hack/build.sh
 
-start:
-	./hack/virt-install-aio-ign.sh ./mydir/aio.ign
+%/install-config.yaml: install-config-%.yaml ./bin/openshift-install
+	rm -rf $*
+	mkdir $*
+	cp ./install-config-$*.yaml $*/install-config.yaml
+
+generate-%: %/install-config.yaml
+	ls -al $*
+	./bin/openshift-install create aio-config --dir=$*
+
+
+start-%: clean-% generate-%
+	@echo Starting $*
+	sudo ./hack/virt-install-aio-ign.sh ./$*/aio.ign $*
+
+start: start-cluster1 start-cluster2
+
+clean: clean-cluster1 clean-cluster2
 
 network:
-	./hack/virt-create-net.sh
+	sudo ./hack/virt-create-net.sh
 
-ssh:
+ssh-%:
 	chmod 400 ./hack/ssh/key
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ./hack/ssh/key core@192.168.126.10
+	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ./hack/ssh/key core@api.$*.redhat.com
+
+nodes-%:
+	kubectl --kubeconfig=./$*/auth/kubeconfig get nodes 
 
 image:
 	curl -O -L https://releases-art-rhcos.svc.ci.openshift.org/art/storage/releases/rhcos-4.6/46.82.202007051540-0/x86_64/rhcos-46.82.202007051540-0-qemu.x86_64.qcow2.gz
--- hack/virt-install-aio-ign.sh
+++ hack/virt-install-aio-ign.sh
@@ -1,29 +1,66 @@
 #!/bin/bash
-
+set -x
 # $ curl -O -L https://releases-art-rhcos.svc.ci.openshift.org/art/storage/releases/rhcos-4.6/46.82.202007051540-0/x86_64/rhcos-46.82.202007051540-0-qemu.x86_64.qcow2.gz
 # $ mv rhcos-46.82.202007051540-0-qemu.x86_64.qcow2.gz /tmp
 # $ sudo gunzip /tmp/rhcos-46.82.202007051540-0-qemu.x86_64.qcow2.gz
 
 IGNITION_CONFIG="/var/lib/libvirt/images/aio.ign"
-sudo cp "$1" "${IGNITION_CONFIG}"
-sudo chown qemu:qemu "${IGNITION_CONFIG}"
-sudo restorecon "${IGNITION_CONFIG}"
+cp "$1" "${IGNITION_CONFIG}"
+chown qemu:qemu "${IGNITION_CONFIG}"
+restorecon "${IGNITION_CONFIG}"
 
 RHCOS_IMAGE="/tmp/rhcos-46.82.202007051540-0-qemu.x86_64.qcow2"
 VM_NAME="aio-test"
+if [ ! -z "$2" ]; then
+    VM_NAME="$2"
+fi
+
 OS_VARIANT="rhel8.1"
 RAM_MB="16384"
 DISK_GB="20"
+DISK="/home/Development/virt/${VM_NAME}.cow"
+rm -f "${DISK}"
 
+LASTOCT=$(echo ${VM_NAME} | md5sum | cut -c 1-2)
+MAC="52:54:00:ee:42:$LASTOCT"
+
+NETWORK=test-net
+
+virsh net-dhcp-leases $NETWORK | grep ${MAC}
+    #if [ $? = 0 ]; then
+virsh net-update $NETWORK delete ip-dhcp-host "<host mac='${MAC}'/>" --live 2>&1 > /dev/null
+    #fi
+
+ip=10
+network=$( virsh net-dumpxml $NETWORK | grep range | awk -F \' '{print $2}' | awk -F . '{print $1"."$2"."$3}' )
+rc=1
+while [ 1 = 1 ]; do
+    virsh net-update $NETWORK add ip-dhcp-host "<host mac='${MAC}' name='${VM_NAME}' ip='${network}.${ip}'/>" --live
+    rc=$?
+    if [ $rc = 0 ]; then
+        break
+    elif [ $ip -gt 254 ]; then
+	echo "No more IPs left"
+        exit 1
+    else
+        ip=$(($ip + 1))
+    fi 
+done
+
+echo "address=/.${VM_NAME}.redhat.com/${network}.${ip}" > /etc/NetworkManager/dnsmasq.d/${VM_NAME}.conf
+sudo systemctl reload NetworkManager.service
+
+echo "Installing ${VM_NAME} ( $MAC ) @ ${network}.${ip}"
+echo 
+echo 
 virt-install \
     --connect qemu:///system \
     -n "${VM_NAME}" \
     -r "${RAM_MB}" \
     --os-variant="${OS_VARIANT}" \
     --import \
-    --network=network:test-net,mac=52:54:00:ee:42:e1 \
+    --network=network:test-net,mac="$MAC" \
     --graphics=none \
-    --disk "size=${DISK_GB},backing_store=${RHCOS_IMAGE}" \
-    --noautoconsole \
+    --disk "path=${DISK},size=${DISK_GB},backing_store=${RHCOS_IMAGE}" \
     --qemu-commandline="-fw_cfg name=opt/com.coreos/config,file=${IGNITION_CONFIG}"
 
